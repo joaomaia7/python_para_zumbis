14f12cf446bf2ea4c008de949a7b9626ae30cf46
João Victor Maia <72816333+joaomaia7@users.noreply.github.com>
2021-01-18 11:54:01 -0300

    Create exercicio2.py

---
+++ exercicio2.py
@@ -0,0 +1,4 @@
+# Escreva um programa que leia um valor em metros e o exiba convertido em milímetros
+
+metro = int(input('Digite o valor em metros: ')) #pede um valor em metros;
+print(f'O valor na conversão em milímetros corresponde a: {metro/1000} mm') #imprime a conversão do valor para milímetros;
