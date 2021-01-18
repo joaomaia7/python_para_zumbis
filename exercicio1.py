7e8e4380b762182cdcead5fcdfbe73f996d6e809
João Victor Maia <72816333+joaomaia7@users.noreply.github.com>
2021-01-18 11:52:52 -0300

    Create exercicio1.py

---
+++ exercicio1.py
@@ -0,0 +1,6 @@
+# Faça um programa que peça dois números inteiros e imprima a soma desses dois números
+
+n1 = int(input('Digite o primeiro número inteiro: ')) # pede o primeiro inteiro;
+n2 = int(input('Digite o segundo número inteiro: ')) # pede o segundo inteiro;
+
+print(f'O resultado da soma entre os dois números é: {n1+n2}.') # imprime a soma dos dois valores;
